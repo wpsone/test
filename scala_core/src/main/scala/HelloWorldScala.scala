197e7ae662bf44718b87d4e2414c7e911ba67d14
wpsone <173897137@qq.com>
2022-02-01 22:41:49 +0800

    scala基础

--- scala_core/src/main/scala/HelloWorldScala.scala
+++ scala_core/src/main/scala/HelloWorldScala.scala
@@ -1,7 +1,16 @@
 object HelloWorldScalaXXXX {
 //HelloWorldScalaXXXX HelloWorldScala 不影响执行 java影响
+  /**
+   * def 用来定义方法的关键字
+   * main 方法名
+   * args 形参名称
+   * Array[String] args这个形参的数据类型,是String类型的数组 等同于java String[]
+   * @param args
+   */
   def main(args: Array[String]): Unit = {
     println("hello world scala")
+
+    val xx1=new Object()
   }
 
 }
