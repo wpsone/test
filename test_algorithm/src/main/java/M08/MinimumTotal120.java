249285d4ff397aa6d677c35f2e87ba1074fb06af
wpsone <173897137@qq.com>
2022-03-06 23:34:18 +0800

    Leetcode算法题练习

--- test_algorithm/src/main/java/M08/MinimumTotal120.java
+++ test_algorithm/src/main/java/M08/MinimumTotal120.java
@@ -42,23 +42,47 @@ public class MinimumTotal120 {
 //    }
 
     //动态规划算法
+//    public int minimumTotal(List<List<Integer>> triangle) {
+//        int n = triangle.size();
+//        int[][] dp = new int[n][n];
+//        for (int[] arr : dp) {
+//            Arrays.fill(arr,Integer.MAX_VALUE);
+//        }
+//        dp[0][0] = triangle.get(0).get(0);
+//        for (int i = 0; i < n - 1; i++) {
+//            for (int j = 0; j <= i; j++) {
+//                dp[i+1][j]=Math.min(dp[i+1][j],dp[i][j]+triangle.get(i+1).get(j));
+//                dp[i+1][j+1]=Math.min(dp[i+1][j+1],dp[i][j]+triangle.get(i+1).get(j+1));
+//            }
+//        }
+//        int ans = Integer.MAX_VALUE;
+//        for (int x :
+//                dp[n - 1]) {
+//            ans = Math.min(x, ans);
+//        }
+//        return ans;
+//    }
+
+    //滚动数组
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int[][] dp = new int[n][n];
+        int[][] dp = new int[2][n];
         for (int[] arr : dp) {
             Arrays.fill(arr,Integer.MAX_VALUE);
         }
         dp[0][0] = triangle.get(0).get(0);
         for (int i = 0; i < n - 1; i++) {
+            int ind = i % 2;
+            int next_ind = 1 - ind;
             for (int j = 0; j <= i; j++) {
-                dp[i+1][j]=Math.min(dp[i+1][j],dp[i][j]+triangle.get(i+1).get(j));
-                dp[i+1][j+1]=Math.min(dp[i+1][j+1],dp[i][j]+triangle.get(i+1).get(j+1));
+                dp[next_ind][j] = Math.min(dp[next_ind][j],dp[ind][j]+triangle.get(i+1).get(j));
+                dp[next_ind][j+1] = Math.min(dp[next_ind][j+1],dp[ind][j]+triangle.get(i+1).get(j+1));
             }
+            Arrays.fill(dp[ind],Integer.MAX_VALUE);
         }
         int ans = Integer.MAX_VALUE;
-        for (int x :
-                dp[n - 1]) {
-            ans = Math.min(x, ans);
+        for (int x : dp[(n - 1) % 2]) {
+            ans=Math.min(x,ans);
         }
         return ans;
     }
