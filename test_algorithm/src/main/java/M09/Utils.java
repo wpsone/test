2e1bba9d4b06c5a0c2119761d34c73c65c50af24
wpsone <173897137@qq.com>
2022-05-13 08:05:43 +0800

    Leetcode算法题练习

---
+++ test_algorithm/src/main/java/M09/Utils.java
@@ -0,0 +1,19 @@
+package M09;
+
+public class Utils {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(){}
+
+        TreeNode(int val) {this.val = val;}
+
+        TreeNode(int val, TreeNode left,TreeNode right) {
+            this.val=val;
+            this.left=left;
+            this.right=right;
+        }
+    }
+}
