89b71176f2e7a062cf763be535b90b93901b0214
wpsone <173897137@qq.com>
2022-05-14 09:54:36 +0800

    Leetcode算法题练习

--- test_algorithm/src/main/java/M09/Utils.java
+++ test_algorithm/src/main/java/M09/Utils.java
@@ -1,5 +1,9 @@
 package M09;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Utils {
     public class TreeNode {
         int val;
@@ -14,6 +18,26 @@ public class Utils {
             this.val=val;
             this.left=left;
             this.right=right;
+        }
+    }
+
+    public static class Node {
+        public int val;
+        public List<Node> neighbors;
+
+        public Node() {
+            val = 0;
+            neighbors = new ArrayList<Node>();
+        }
+
+        public Node(int _val) {
+            val = _val;
+            neighbors = new ArrayList<>();
+        }
+
+        public Node(int _val,ArrayList<Node> _neighbors) {
+            val = _val;
+            neighbors = _neighbors;
         }
     }
 }
